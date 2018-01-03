351ae3445b767065443973b769326d360babc324
Grant Seastream <gseastre@andrew.cmu.edu>
2018-01-03 02:57:48 -0500

    Added slideshow, added Anthony, changed nav bar, some brother updates

---
+++ assets/js/slideshow.js
@@ -0,0 +1,37 @@
+var slideIndex = 0;
+var timerid;
+carousel();
+
+function plusDivs(n) {
+    clearTimeout(timerid);
+    showDivs(slideIndex += n);
+    timerid = setTimeout(carousel, 10000); // 10 seconds after manual
+}
+
+function currentDiv(n) {
+    clearTimeout(timerid);
+    showDivs(slideIndex = n);
+    timerid = setTimeout(carousel, 10000);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dots");
+    if (n > x.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " w3-white";
+}
+
+
+function carousel() {
+    showDivs(slideIndex += 1);
+    timerid = setTimeout(carousel, 5000); // 5 seconds auto
+}
